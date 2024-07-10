c0fdba6ec883ca766c2ec184b03fd85eb15ec6f3
saimadhav2003 <saimadhav1699@gmail.com>
2024-07-10 16:47:11 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,23 @@
+marks = int(input("enter your marks(0 to 100)"))
+if marks>=90:
+    grade="Excelent"   
+elif marks>=80:
+    grade="A++"     
+elif marks>=70:
+    grade="A"
+elif marks>=60:
+    grade="B"    
+elif marks>=50:
+    grade="c" 
+elif marks<50:
+    grade="Fail"  
+print(grade)
+
+ 
+                
+                  
+                
+
+
+
+    
